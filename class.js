533d9e014a9bf2f7a565e1753e5a1bc56370c2d9
Prabhatebook <163877065+Prabhatebook@users.noreply.github.com>
2024-03-29 11:29:00 +0530

    Update class.js

---
+++ class.js
@@ -0,0 +1,33 @@
+document.getElementById("btn-1").addEventListener("click", function() {
+    window.location.href = "./StudyMaterial/nursery.html";
+});
+document.getElementById("btn-2").addEventListener("click", function() {
+    window.location.href = "./StudyMaterial/LKG.html";
+});
+document.getElementById("btn-3").addEventListener("click", function() {
+    window.location.href = "./StudyMaterial/UKG.html";
+});
+document.getElementById("btn-4").addEventListener("click", function() {
+    window.location.href = "./StudyMaterial/Class-1.html";
+});
+document.getElementById("btn-5").addEventListener("click", function() {
+    window.location.href = "./StudyMaterial/Class-2.html";
+});
+document.getElementById("btn-6").addEventListener("click", function() {
+    window.location.href = "./StudyMaterial/Class-3.html";
+});
+document.getElementById("btn-7").addEventListener("click", function() {
+    window.location.href = "./StudyMaterial/Class-4.html";
+});
+document.getElementById("btn-8").addEventListener("click", function() {
+    window.location.href = "./StudyMaterial/Class-5.html";
+});
+document.getElementById("btn-9").addEventListener("click", function() {
+    window.location.href = "./StudyMaterial/Class-6.html";
+});
+document.getElementById("btn-10").addEventListener("click", function() {
+    window.location.href = "./StudyMaterial/Class-7.html";
+});
+document.getElementById("btn-11").addEventListener("click", function() {
+    window.location.href = "./StudyMaterial/Class-8.html";
+});
